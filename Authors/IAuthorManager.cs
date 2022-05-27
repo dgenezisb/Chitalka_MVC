0b537851c119d09c572dd255249a65122053b1bc
Vov4ikINR <64402712+dgenezisb@users.noreply.github.com>
2022-05-28 01:49:13 +0300

    Delete Authors directory

--- Authors/IAuthorManager.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ChitalkaMVC.Logic.Authors
-{
-    public interface IAuthorManager
-    {
-        Task<IList<Author>> GetAll();
-        Task Create();
-        Task Delete(int id);
-    }
-}
